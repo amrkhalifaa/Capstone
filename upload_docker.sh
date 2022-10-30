e71ac1e390498d96e038f3a8135e6bde4af900b9
amrkhalifaa <you@example.com git config --global user.email amrkhalifaa95@gmail.com>
2022-10-30 22:16:21 +0200

    test

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,17 +5,16 @@
 # This file was used to push the original App docker image to AWS ECR
 
 # Step 1:
-# Authenticate
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273801816274.dkr.ecr.us-east-1.amazonaws.com
+# Authenticateaws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 016957448722.dkr.ecr.us-east-1.amazonaws.com
 
 # Step 2:
 # Build image
-docker build -t web-app:1.0 .
+docker build -t khalifacapstone .
 
 # Step 2:  
 # Tag the image to push it to the repo
-docker tag web-app:1.0 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker tag khalifacapstone:latest 016957448722.dkr.ecr.us-east-1.amazonaws.com/khalifacapstone:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker push 016957448722.dkr.ecr.us-east-1.amazonaws.com/khalifacapstone:latest
