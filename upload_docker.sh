d60e864dcd921752ff9174165b6dfb614ed87014
amrkhalifaa <you@example.com git config --global user.email amrkhalifaa95@gmail.com>
2022-11-01 16:28:11 +0200

    kamal code

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,15 +6,16 @@
 
 # Step 1:
 # Authenticate
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 350681124025.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 273801816274.dkr.ecr.us-east-1.amazonaws.com
+
 # Step 2:
 # Build image
 docker build -t web-app:1.0 .
 
 # Step 2:  
 # Tag the image to push it to the repo
-docker tag web-app:1.0 350681124025.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker tag web-app:1.0 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
 
 # Step 3:
 # Push image to a docker repository
-docker push 350681124025.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
+docker push 273801816274.dkr.ecr.us-east-1.amazonaws.com/devops-project:web-app-1.0
